373972c227124bda1d2f8a3e51061ecd49378e24
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-13 12:55:46 -0700

    add keys and url to config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
-export const POSTHOG_PUBLIC_KEY = ""
-export const POSTHOG_URL = ""
-export const AI_ENDPOINT = ""
+export const POSTHOG_PUBLIC_KEY =
+  "phc_Ejf4Ve7ssSEbL7dw80qC4XHK2l5CRCKfrvcPuGc97pp"
+export const POSTHOG_URL = "https://d38u8ubrx3bbto.cloudfront.net"
+export const AI_ENDPOINT =
+  "https://4ih4l949q0.execute-api.us-east-2.amazonaws.com/default/openai"
