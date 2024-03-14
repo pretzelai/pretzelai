d096f21cfb4f18597b3eee4e28317f1270b3e20f
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-14 09:43:14 -0700

    update AI_ENDPOINT to US West region

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ export const POSTHOG_PUBLIC_KEY =
   "phc_Ejf4Ve7ssSEbL7dw80qC4XHK2l5CRCKfrvcPuGc97pp"
 export const POSTHOG_URL = "https://d38u8ubrx3bbto.cloudfront.net"
 export const AI_ENDPOINT =
-  "https://4ih4l949q0.execute-api.us-east-2.amazonaws.com/default/openai"
+  "https://k51dgywbyi.execute-api.us-west-1.amazonaws.com/default/openai_west"
