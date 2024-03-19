a81fef60b960816622e5cccd9fe5033e73cef34f
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-18 18:41:52 -0700

    update server and posthog

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const POSTHOG_PUBLIC_KEY =
-  "phc_Ejf4Ve7ssSEbL7dw80qC4XHK2l5CRCKfrvcPuGc97pp"
-export const POSTHOG_URL = "https://d38u8ubrx3bbto.cloudfront.net"
+  "phc_4kei41nB5Nc5Deih7XnWAzSjgmlAUxFCgS1QaYrSFGH"
+export const POSTHOG_URL = "https://d2yfaqny8nshvd.cloudfront.net"
 export const AI_ENDPOINT =
   "https://k51dgywbyi.execute-api.us-west-1.amazonaws.com/default/openai_west"
