6e3867cab93e1115fcff3a2a6c82a1deac59dfe9
Ramon Garate <ramon@withpretzel.com>
2024-06-07 12:38:14 +0200

    add shortcuts and focus to input box and improve UI

--- packages/pretzelai-extension/src/chat.tsx
+++ packages/pretzelai-extension/src/chat.tsx
@@ -11,9 +11,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
 import { LabIcon } from '@jupyterlab/ui-components';
 import pretzelSvg from '../style/icons/pretzel.svg';
-import { Box, Button, IconButton, TextField, Typography } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import StopIcon from '@mui/icons-material/Stop';
+import { Box, TextField, Typography } from '@mui/material';
 import { CHAT_SYSTEM_MESSAGE, chatAIStream } from './chatAIUtils';
 import { ChatCompletionMessage } from 'openai/resources';
 import { INotebookTracker } from '@jupyterlab/notebook';
@@ -180,7 +178,11 @@ export function Chat({
   }, [messages]);
 
   const onSend = async () => {
+    if (input.trim() === '' || isAiGenerating) {
+      return;
+    }
     setIsAiGenerating(true);
+    const inputMarkdown = input.replace(/\n/g, '  \n');
     const activeCellCode = notebookTracker?.activeCell?.model?.sharedModel?.source;
     const embeddings = await readEmbeddings(notebookTracker, app);
     const selectedCode = getSelectedCode(notebookTracker).extractedCode;
@@ -199,7 +201,7 @@ export function Chat({
 
     const newMessage = {
       id: String(messages.length + 1),
-      content: input,
+      content: inputMarkdown,
       role: 'user'
     };
 
@@ -239,11 +241,20 @@ export function Chat({
     });
   };
 
+  const cancelGeneration = () => {
+    setIsAiGenerating(false);
+    stopGeneration();
+    setReferenceSource('');
+  };
+
   function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && !event.shiftKey) {
       onSend();
       event.stopPropagation();
       event.preventDefault();
+    }
+    if (event.key === 'Escape') {
+      cancelGeneration();
     }
   }
 
@@ -319,48 +330,39 @@ export function Chat({
         ))}
         <div ref={messagesEndRef} />
       </Box>
-      {isAiGenerating ? (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 1 }}>
-          <Typography>Generating AI response...</Typography>
-          <IconButton
-            onClick={() => {
-              setIsAiGenerating(false);
-              stopGeneration();
-              setReferenceSource('');
-            }}
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', padding: 1 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <TextField
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            onKeyDown={handleKeyDown}
+            fullWidth
+            multiline
+            rows={4}
+            placeholder="Type message to ask AI..."
+            autoComplete="off"
             sx={{
-              color: 'red',
-              backgroundColor: '#ffcccc',
-              borderRadius: '10%',
-              fontSize: '1rem',
-              alignSelf: 'center'
+              color: 'var(--jp-ui-font-color1)',
+              '& .MuiInputBase-input': {
+                color: 'var(--jp-ui-font-color1)'
+              },
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'var(--jp-ui-font-color1)'
+              }
             }}
-          >
-            <StopIcon />
-            Cancel
-          </IconButton>
+          />
         </Box>
-      ) : (
-        <Box sx={{ display: 'flex', flexDirection: 'column', padding: 1 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <TextField
-              value={input}
-              onChange={e => setInput(e.target.value)}
-              onKeyDown={handleKeyDown}
-              fullWidth
-              placeholder="Type message to ask AI..."
-              autoComplete="off"
-              sx={{
-                color: 'var(--jp-ui-font-color1)',
-                '& .MuiInputBase-input': {
-                  color: 'var(--jp-ui-font-color1)'
-                },
-                '& .MuiOutlinedInput-notchedOutline': {
-                  borderColor: 'var(--jp-ui-font-color1)'
-                }
-              }}
-            />
+        {isAiGenerating ? (
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+            <button className="remove-button" onClick={cancelGeneration} title="Cancel">
+              Cancel <span style={{ fontSize: '0.8em' }}>Esc</span>
+            </button>
+            <Typography sx={{ marginRight: 'var(--jp-ui-margin, 10px)', marginTop: 'var(--jp-ui-margin, 10px)' }}>
+              Generating AI response...
+            </Typography>
           </Box>
+        ) : (
           <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
             <button className="pretzelInputSubmitButton" onClick={onSend} title="Submit ↵">
               Submit <span style={{ fontSize: '0.8em' }}>↵</span>
@@ -378,8 +380,8 @@ export function Chat({
               {'>'}
             </button>
           </Box>
-        </Box>
-      )}
+        )}
+      </Box>
     </Box>
   );
 }
