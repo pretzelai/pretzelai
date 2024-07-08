dc1aad53e4cad8b420cefbfd2059a27ade164557
Prasoon Shukla <prasoon@withpretzel.com>
2024-07-08 20:49:01 +0200

    add copyright notice

--- packages/pretzelai-extension/src/migrations/defaultSettings.ts
+++ packages/pretzelai-extension/src/migrations/defaultSettings.ts
@@ -1,4 +1,13 @@
 /* eslint-disable camelcase */
+/*
+ * Copyright (c) Pretzel AI GmbH.
+ * This file is part of the Pretzel project and is licensed under the
+ * GNU Affero General Public License version 3.
+ * See the LICENSE_AGPLv3 file at the root of the project for the full license text.
+ * Contributions by contributors listed in the PRETZEL_CONTRIBUTORS file (found at
+ * the root of the project) are licensed under AGPLv3.
+ */
+
 import { returnDefaults_1_1 } from './migrate_1_0_to_1_1';
 
 export const getDefaultSettings = (version: string) => {
